3feadaed5ce322bdb0573902ad0c0ef1b7cc231b
Tonyisbuilding <anthonyadewuyi01@gmail.com>
2025-09-25 16:33:08 +0100

    new

--- server/app.js
+++ server/app.js
@@ -33,4 +33,13 @@ app.use('/api/v1/edgeCapital', contactUsEdge);
 
 
 
+const PORT = process.env.PORT || 5000;
+
+if (require.main === module) {
+  app.listen(PORT, () => {
+    console.log(`Server running locally at http://localhost:${PORT}`);
+  });
+}
+
+
 module.exports = app;
